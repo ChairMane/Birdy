657d7f4a285781b55ac417a66ed2fdb4570037ab
ChairMane <danielschris899@gmail.com>
2018-03-22 22:14:06 -0700

    Added new birds and <> rand functionality

--- Birdy.py
+++ Birdy.py
@@ -8,49 +8,61 @@ config = json.load(open('config.json'))
 
 client = discord.Client()
 
+@client.event
+async def error_handle():
+    await client.send_message(message.channel, "Invalid command entered. Please enter `<> help` for commands.")
+
 #rand() will grab a random bird from the image_dict dictionary, using a random key.
 #For example, if you want a random bird, you write '<> rand'
 @client.event
 async def rand(content, image_dict, message):
-    #list(image_dict.values())[0]
-    '''if len(content) > 2:
-        family_name = ' '.join(content[1:].lower()
+
+    if (len(content) > 1):
+
+        family_name = ' '.join(content[1:]).lower()
+        get_family = species_by_family[family_name]
+        rand_bird = random.choice(get_family)
         
-        rand_key = random.choice(list(image_dict))      
-        name, species, desc, filename_list = image_dict[rand_key]
+        name, species, desc, filename_list = image_dict[rand_bird]
         embed = discord.Embed(title='Name', description=species, color=0x6606BA)
         embed.add_field(name="Description", value=desc, inline=False) 
         await client.send_file(message.channel, get_random(filename_list))
         await client.send_message(message.channel, embed=embed)                
-    else:'''
-    rand_key = random.choice(list(image_dict))      
-    name, species, desc, filename_list = image_dict[rand_key]
-    embed = discord.Embed(title='Name', description=species, color=0x6606BA)
-    embed.add_field(name="Description", value=desc, inline=False) 
-    await client.send_file(message.channel, get_random(filename_list))
-    await client.send_message(message.channel, embed=embed)
+
+    else:
+        
+        rand_key = random.choice(list(image_dict))
+        name, species, desc, filename_list = image_dict[rand_key]
+        
+        embed = discord.Embed(title='Name', description=species, color=0x6606BA)
+        embed.add_field(name="Description", value=desc, inline=False) 
+        await client.send_file(message.channel, get_random(filename_list))
+        await client.send_message(message.channel, embed=embed)
+
 
 #This function is called when the user knows the name of the bird they want information about
 #For example, if you want information on the white crowned sparrow, you write '<> white crowned sparrow'    
 @client.event
 async def get_species(content, image_dict, message):
-    usr_msg = ' '.join(content[0:]).lower()
+    usr_msg = ' '.join(content).lower()
     print(usr_msg)
     name, species, desc, filename_list = image_dict[usr_msg]
     embed = discord.Embed(title='Name', description=species, color=0x6606BA)
     embed.add_field(name="Description", value=desc, inline=False) 
     await client.send_file(message.channel, get_random(filename_list))
     await client.send_message(message.channel, embed=embed) 
+
     
 @client.event
 async def handle_command(content, message, image_dict):
 
-    if len(content) == 1:
-        return "ERROR, MISSING COMMAND"
+    if len(content) == 0:
+        await error_handle()
     elif content[1] == 'rand':
         await rand(content[1:], image_dict, message)
     elif content[1] != 'rand':
         await get_species(content[1:], image_dict, message)
+
 
 def get_random(mylist):
     r = random.randrange(len(mylist))
@@ -64,10 +76,9 @@ async def on_message(message):
         return
     
     content = message.content.split(' ')
-    
+
     if '<>' != content[0]:
         return
-        #await client.send_message(message.channel, 'Invalid Command. Please use `<> help` for list of commands')
 
     if '<>' == content[0]:
         #send to handle_command() to do something with
